9c9ddaa0a07bd1fd17d6b611aec4aa9b92e22ae1
Hritik Singh <hritiksingh812@gmail.com>
2020-05-19 13:20:30 +0530

    "Final App"

--- TodoApp/Todo/urls.py
+++ TodoApp/Todo/urls.py
@@ -11,10 +11,6 @@ urlpatterns = [
     url(r'dailyworks-add/$', views.DailyWorksCreate.as_view(), name='dailyworks_add'),
 
     # /Todo/dailyworks/2/delete/
-url(r'^(?P<work_id>[0-9]+)/delete_dailyworks/$', views.delete_dailyworks, name='delete_dailyworks'),
+    url(r'^(?P<work_id>[0-9]+)/delete_dailyworks/$', views.delete_dailyworks, name='delete_dailyworks'),
 
-    #url(r'^(P<pk>[0-9]+)/$')
-    # this url will redirect us to index page after deleting the selected Task
-   # url(r'dailyworks/delete/(?P<pk>[0-9]+)/delete/$', views.DailyWorksDelete.as_view(), name='dailyworks_delete'),
-    #url(r'^dailyworks_delete', views.remove, name="dailyworks_delete"),
 ]
